860ae050377b401f737eb6b93fe8c757eb1782f4
James Hoggard <jayrh3246@gmail.com>
2021-01-28 16:24:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,9 @@
 first_number = 9
 second_number = 1
 sum = first_number + second_number
-difference = 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
 
 
 
